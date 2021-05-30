3ca6b4f96754028003fa44fc5f1a962bf194e416
hunterD3CD <hunterd3c@gmail.com>
2021-05-30 13:42:31 -0700

    javaupdate_question array

--- script.js
+++ script.js
@@ -2,3 +2,42 @@ var viewScore = document.querySelector("#viewScore");
 viewScore.addEventListener("click", function () {
   alert("button click");
 });
+// --------------------------Create Questions Array-----------------------------------
+var quizQuestions = [
+  {
+    question: "Which of the following is an advantage of using JavaScript?",
+    choiceA: "less server interaction",
+    choiceB: "immediate feedback to the visitors",
+    choiceC: "increased interactivity",
+    choiceD: "all of the above",
+    correctAnswer: "C",
+  },
+  {
+    question:
+      "Which built-in method calls a function for each element in the array?",
+    choiceA: "while()",
+    choiceB: "loop()",
+    choiceC: "forEach()",
+    choiceD: "None of the above",
+    correctAnswer: "C",
+  },
+  {
+    question:
+      "Which of the following function of String object returns the calling string value converted to lower case?",
+    choiceA: "toLocaleLowerCase()",
+    choiceB: "toLowerCase()",
+    choiceC: "toString()",
+    choiceD: "substring()",
+    correctAnswer: "B",
+  },
+  {
+    question:
+      "Which of the following function of Array object returns a new array comprised of this array joined with other array(s) and/or value(s)?",
+    choiceA: "concat()",
+    choiceB: "pop()",
+    choiceC: "push()",
+    choiceD: "some()",
+    correctAnswer: "A",
+  },
+];
+console.log(quizQuestions[2]);
